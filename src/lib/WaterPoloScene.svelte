3da65e329966daef38deb758a6fa6f8ffcf88d60
Roberts Pelle <roberts.pelle@gmail.com>
2024-04-19 19:13:36 +0300

    #1 - Izveidota vienkārša pamata 3D vietne ar A-Frame (kuru var atvērt ar Github Pages)

---
+++ src/lib/WaterPoloScene.svelte
@@ -0,0 +1,9 @@
+<script>
+  // import { AFRAME } from "aframe";
+  import "aframe";
+</script>
+
+<a-scene>
+  <a-box position="0 1.5 -3" rotation="0 45 0" color="#4CC3D9"></a-box>
+  <a-sky color="#ECECEC"></a-sky>
+</a-scene>
